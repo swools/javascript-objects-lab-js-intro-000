9d574a1e69db89f2f83d565e7adb6f71b322d0e5
Stephen Wool <stevewool7@gmail.com>
2019-03-02 15:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, key = value)
-  
-}
